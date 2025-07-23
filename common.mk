5278c9ac8d02325c9227990487fb07d29b03bdc4
tilt-rgb <lesma_chan@pm.me>
2025-07-23 16:57:59 +0000

    revert: sm7325-common: Add flash control app - App to modify flash intesity
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>
    Co-authored-by: Soo-Hwan Na <whiteshell2544@naver.com>

--- common.mk
+++ common.mk
@@ -409,11 +409,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
-# Samsung Ext Flash
-PRODUCT_PACKAGES += \
-    vendor.samsung_ext.hardware.camera.flashlight-service \
-    FlashControl
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
@@ -464,7 +459,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/samsung-ext/interfaces \
     hardware/samsung
 
 # Inherit proprietary blobs
